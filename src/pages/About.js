69dc78df64df6966aac15a5c6f459377cd7b05aa
Tadas Baltrunas <tadas@baltrunas.lt>
2024-09-28 21:37:46 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+// src/pages/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Hi, I’m Tadas Baltrūnas, a software engineering student. I love exploring new technologies and building projects.</p>
+      <p>This blog is my journey through coding, where I share what I learn and create.</p>
+    </div>
+  );
+}
+
+export default About;
